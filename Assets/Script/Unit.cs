4405038aa39a5e1b6b048bb3bf496d220915ec78
Grzegorz Kazimierczak <grzegorz.kazimierczak.sg@gmail.com>
2023-07-06 19:08:21 +0200

    Basic UnitMovement (Unit.cs)

---
+++ Assets/Script/Unit.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class Unit : MonoBehaviour
+{
+    private Vector3 targetPosition;
+    [SerializeField] private float moveSpeed = 4f;
+    [SerializeField] private float stoppingDistance = 0.1f;
+
+    private void Update()
+    {
+        if (Vector3.Distance(targetPosition, transform.position) > stoppingDistance)
+        {
+            Vector3 moveDirection = (targetPosition - transform.position).normalized;
+            transform.position += moveDirection * Time.deltaTime * moveSpeed;
+        }
+        else
+        {
+            transform.position = targetPosition;
+        }
+
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            Move(new Vector3(4, 0, 4));
+        }
+
+
+    }
+
+
+    private void Move(Vector3 targetPosition)
+    {
+        this.targetPosition = targetPosition;
+    }
+}
